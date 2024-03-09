9600e3f9251d31ce25c63ba966cb5cafc9ec5a0e
Owen Lovett <olovett214@gmail.com>
2024-03-08 21:53:15 -0800

    added ground

--- Assets/Scripts/EnemyObject.cs
+++ Assets/Scripts/EnemyObject.cs
@@ -7,5 +7,9 @@ public class EnemyObject : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D coll)
     {
         Destroy(this.gameObject);
+        if (coll.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("Hit Ground");
+        }
     }
 }
